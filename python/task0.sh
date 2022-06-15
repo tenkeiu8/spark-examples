3e2beb88e7de54d27ae92e3c6f5f536d136818ac
tenkeiu8 <tenke.iu8@gmail.com>
2022-06-15 14:50:43 +0300

    Delete python directory

--- python/task0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PYSPARK_PYTHON=python3.7 spark-submit \
-    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:conf/log4j.properties" \
-    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:conf/log4j.properties" \
-    --master local[*] \
-    python/task0.py
-
-
